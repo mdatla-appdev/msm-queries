3b64f2ff9ec2b445c8fc5b3fffdfc61c4a55cee4
Manasa Datla <mdatla@chicagobooth.edu>
2023-04-18 18:00:32 +0000

    Director ID complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,8 +2,29 @@ class DirectorsController < ApplicationController
 
   def index
     @list_of_directors = Director.all
-    
+
   render ({ :template => "directors_templates/index.html.erb" })
+
+  end
+
+  def wisest
+
+    @oldest = Director.where.not({ :dob => nil}).order({ :dob => :asc}).at(0)
+  
+    render ({ :template => "directors_templates/wisest.html.erb" })
+
+  end
+
+  def directors_details
+
+    # params looks like {"an_id"=>"42"}
+
+  @the_id = params.fetch("an_id")
+
+  Director.where({ :id => the_id })
+  
+  
+    render ({ :template => "directors_templates/show.html.erb" })
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors" , { :controller => "directors" , :action => "index"}) 
+
+  get("/directors/eldest", { :controller => "directors" , :action => "wisest"})
+
+  get("/directors/:an_id" , { :controller => "directors" , :action => "directors_details"})
+
+  get("/", { :controller => "application", :action => "homepage" })
+
+
+
+
+
+ 
+
+
+
 end
